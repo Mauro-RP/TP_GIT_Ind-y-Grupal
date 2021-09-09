116ffa95338e09171f19e531f2bf68f1552d18e6
Mauro-RP <mauro.r.prieto@gmail.com>
2021-09-09 20:57:58 -0300

    a mitad de camino

--- Suma_Nros_Aleatorios.php
+++ Suma_Nros_Aleatorios.php
@@ -1,4 +1,16 @@
 <?php
 /* Dados como datos 200 números enteros (hacerlo aleatorios entre -200 y 200),
  obtener y mostrar su suma.*/
+function Conjunto($n)
+{
+    $conjunto=array();
+    for ($i=0; $i < $n; $i++) 
+    { 
+        $conjunto[$i] = rand(-200,200);
+    }
+    return $conjunto;
+}
+
+$enteros = Conjunto(200);
+
 ?>
